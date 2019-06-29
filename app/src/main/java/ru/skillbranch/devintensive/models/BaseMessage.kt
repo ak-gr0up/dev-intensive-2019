b9fb947d7aac5771dc8a819b2111c1970cf7a17c
Dmitry <dkoptev06@mail.ru>
2019-06-29 17:25:14 +0300

    second task completed

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -9,5 +9,16 @@ abstract class BaseMessage(
     val isIncoming: Boolean = false,
     val date: Date = Date()
 ) {
-    fun formatMessage(): String = "$id, $from, $isIncoming, $"
+    abstract fun formatMessage(): String
+    companion object AbstractFactory{
+        var lastId: Int = -1
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type:String? = "text", payload:Any?): BaseMessage{
+            lastId ++
+            return when(type){
+                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String)
+                    else -> TextMessage("$lastId", from, chat, date = date, text = payload as String)
+            }
+
+        }
+    }
 }

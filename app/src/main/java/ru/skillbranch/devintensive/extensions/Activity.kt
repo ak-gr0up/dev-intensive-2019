d1430977b9171cd29c6948d2557bfe4d326892b1
Dmitry <dkoptev06@mail.ru>
2019-07-15 19:54:26 +0300

    validation completed

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,9 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.MainActivity
+
+class Activity {
+    fun hideKeyboard(): Unit{
+
+    }
+}
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -4,23 +4,71 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 
     var wrongAnswers: Int = 0
 
-
-    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>>{
-        if (question.answers.contains(answer)){
-            question = question.nextQuestion()
-            return "Отлично - ты справился\n${question.question}" to status.color
-        }
-        else{
-            if (wrongAnswers == 3){
-                wrongAnswers = 0
-                status =  Status.NORMAL
-                question = Question.NAME
-                return "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+    fun validation(answer: String): String{
+        if (question == Question.NAME){
+            return when(answer[0].toUpperCase() == answer[0]){
+                true -> ""
+                false -> "Имя должно начинаться с заглавной буквы"
             }
-            wrongAnswers ++
-            status = status.nextStatus()
-            return "Это неправильный ответ\n${question.question}" to status.color
         }
+        else if (question == Question.PROFESSION){
+            return when(answer[0].toLowerCase() == answer[0]){
+                true -> ""
+                false -> "Профессия должна начинаться со строчной буквы"
+            }
+        }
+        else if (question == Question.MATERIAL){
+            return when('0' in answer || '1' in answer || '2' in answer || '3' in answer || '4' in answer ||
+                    '5' in answer || '6' in answer || '7' in answer || '8' in answer || '9' in answer){
+                true -> "Материал не должен содержать цифр"
+                false -> ""
+            }
+
+        }
+        else if (question == Question.BDAY || question == Question.SERIAL){
+            var allLetters = true
+            for (i in (0..answer.length - 1)){
+                if (answer[i] != '0' && answer[i] != '1' && answer[i] != '2' && answer[i] != '3' && answer[i] != '4' &&
+                    answer[i] != '5' && answer[i] != '6' && answer[i] != '7' && answer[i] != '8' && answer[i] != '9'){
+                    allLetters = false
+                    break
+                }
+            }
+            return when(allLetters){true -> "" false -> when(question == Question.BDAY){true ->"Год моего рождения должен содержать только цифры" false -> "Серийный номер содержит только цифры, и их 7"}}
+        }
+        return ""
+    }
+    fun listenAnswer(answerToUp: String): Pair<String, Triple<Int, Int, Int>>{
+        val checkValidation = validation(answerToUp)
+        if (checkValidation == ""){
+            val answer = answerToUp.toLowerCase()
+            if (question.answers.contains(answer)){
+                question = question.nextQuestion()
+                return "Отлично - ты справился\n${question.question}" to status.color
+            }
+            else {
+                if (wrongAnswers == 3){
+                    wrongAnswers = 0
+                    status =  Status.NORMAL
+                    question = Question.NAME
+                    return "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                }
+                if (question != Question.IDLE) {
+                    wrongAnswers ++
+                    status = status.nextStatus()
+                    return "Это неправильный ответ\n${question.question}" to status.color
+                }
+                else{
+                    return "На этом все, вопросов больше нет" to status.color
+                }
+
+                }
+            }
+
+        else{
+            return "${checkValidation}\n${question.question}" to status.color
+        }
+
     }
 
     enum class Status(val color: Triple<Int, Int, Int>) {
@@ -32,14 +80,22 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         fun nextStatus(): Status = values()[(ordinal + 1) % Status.values().size]
     }
     enum class Question(val question: String, val answers: List<String>) {
-        NAME("Как меня зовут?", listOf("Бендер", "bender")),
+        NAME("Как меня зовут?", listOf("бендер", "bender")),
         PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
         MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")),
         BDAY("Когда меня создали?", listOf("2993")),
         SERIAL("Мой серийный номер?", listOf("2716057")),
         IDLE("На этом все, вопросов больше нет", listOf());
 
-        fun nextQuestion() : Question = values()[(ordinal + 1) % Question.values().size]
+        fun nextQuestion() : Question{
+            if (ordinal + 1 == 7){
+                return IDLE
+            }
+            else{
+                return values()[(ordinal + 1)]
+            }
+
+        }
 
     }
 }

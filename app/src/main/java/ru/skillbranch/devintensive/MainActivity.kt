5fc0da204ed5862b4cac58d59d5cb2fcc7f9a19b
Dmitry <dkoptev06@mail.ru>
2019-07-16 22:22:04 +0300

    button Done completed

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -7,16 +7,19 @@ import android.graphics.PorterDuff
 import android.util.Log
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.extensions.hideKeyboard
 import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, TextView.OnEditorActionListener  {
     lateinit var benderImage: ImageView
     lateinit var textTxt: TextView
     lateinit var messageEt: EditText
@@ -41,6 +44,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         textTxt.text = benderObj.question.question
         sendBtn.setOnClickListener(this)
+        messageEt.setOnEditorActionListener(this)
+
+    }
+
+    override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+        if(actionId == EditorInfo.IME_ACTION_DONE){
+            println("yes")
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString())
+            val (r,g,b) = color
+            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+            hideKeyboard()
+            return true
+        }
+        else
+            return false
     }
 
     override fun onClick(v: View?) {
@@ -58,6 +77,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         outState?.putString("STATUS", benderObj.status.name)
         outState?.putString("QUESTION", benderObj.question.name)
     }
+
+
 
 
     }

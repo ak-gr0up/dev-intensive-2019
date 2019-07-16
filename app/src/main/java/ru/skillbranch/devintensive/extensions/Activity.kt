7ed063d9a13f5c8e1a89cecf608aeb6c903df08b
Dmitry <dkoptev06@mail.ru>
2019-07-17 00:18:39 +0300

    try to pass isKeyBoardOpened 3

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -10,6 +10,13 @@ fun Activity.hideKeyboard() {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
+fun Activity.isKeyboardOpened(): Boolean{
+    var rect = Rect()
+    this.window.decorView.getWindowVisibleDisplayFrame(rect)
+    val leftArea: Int = rect.height()
+    val windowHeight: Int = this.resources.displayMetrics.heightPixels
+    return when(windowHeight - leftArea > 250){true -> true; false -> false}
+}
 fun Activity.isKeyboardOpen(): Boolean{
     var rect = Rect()
     this.window.decorView.getWindowVisibleDisplayFrame(rect)
@@ -18,5 +25,5 @@ fun Activity.isKeyboardOpen(): Boolean{
     return when(windowHeight - leftArea > 250){true -> true; false -> false}
 }
 fun Activity.isKeyboardClosed(): Boolean{
-    return when(isKeyboardOpen()){true -> false; false -> true}
+    return when(isKeyboardOpened()){true -> false; false -> true}
 }
